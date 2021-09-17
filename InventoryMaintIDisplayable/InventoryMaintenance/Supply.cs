9f00643d74a7e392f6a1239e240a2a9bdeb29aea
Kelli Porter <77407178+portekn@users.noreply.github.com>
2021-09-17 10:14:10 -0400

    Adding Files by Upload

---
+++ InventoryMaintIDisplayable/InventoryMaintenance/Supply.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryMaintenance
+{
+    public class Supply : InvItem
+    {
+        public Supply() { }
+
+        public Supply(int itemNo, string description, decimal price, string manufacturer) :
+            base(itemNo, description, price)
+        {
+            Manufacturer = manufacturer;
+        }
+
+        public string Manufacturer { get; set; }
+
+        public override string GetDisplayText() =>
+            $"{ItemNo}    {Manufacturer} {Description} ({Price:c})";
+    }
+}
